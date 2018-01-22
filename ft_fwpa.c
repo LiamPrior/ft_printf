562033c56b9515a1c2b34a2688369f30738b6e3f
Liam Prior <liamprior44@gmail.com>
2018-01-21 16:18:59 -0800

    args function

--- ft_fwpa.c
+++ ft_fwpa.c
@@ -6,19 +6,41 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 12:12:06 by lprior            #+#    #+#             */
-/*   Updated: 2018/01/20 14:47:55 by lprior           ###   ########.fr       */
+/*   Updated: 2018/01/21 16:18:36 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-.0
+
+// void	arguments(char *f, int *i, t_flags *bag)
+// {
+// 	while (f[*i] != '\0' && (f[*i] == 'h' || f[*i] == 'l' || f[*i] == 'z' ||
+// 			f[*i] == 'j' || f[*i] == 'q'))
+// 	{
+// 		if (f[*i] == 'h' && ARGUMENT == 1)
+// 			ARGUMENT = 2;
+// 		else if (f[*i] == 'h' && ARGUMENT != 6 && ARGUMENT != 5)
+// 			ARGUMENT = 1;
+// 		else if (f[*i] == 'l' && ARGUMENT == 3)
+// 			ARGUMENT = 4;
+// 		else if (f[*i] == 'l')
+// 			ARGUMENT = 3;
+// 		else if (f[*i] == 'j')
+// 			ARGUMENT = 5;
+// 		else if (f[*i] == 'z')
+// 			ARGUMENT = 6;
+// 		else if (f[*i] == 'q')
+// 			ARGUMENT = 7;
+// 		*i += 1;
+// 	}
+// }
 
 void    width(char *format, int *i, t_flags *tools)
 {
     char *ptr;
 
     ptr = format;
-    while (ft_isdigit(format[*i] && (format[*i] != '\0'))
+    while (ft_isdigit(format[*i]) && format[*i] != '\0')
     {
         format += *i;
         tools->width = ft_atoi(ptr);
@@ -37,12 +59,12 @@ void    ft_flags(char *format, int *i, t_flags *tools)
             tools->negative = 1;
             tools->zero = 0;
         }
-        if (format[*i] == '+')
+        else if (format[*i] == '+')
         {
             tools->positive = 1;
             tools->space = 0;
         }
-        if (format[*i] == ' ' && tools->positive == 0)
+        else if (format[*i] == ' ' && tools->positive == 0)
             tools->space = 1;
         else if (format[*i] == '#')
             tools->hash = 1;
@@ -52,3 +74,33 @@ void    ft_flags(char *format, int *i, t_flags *tools)
     }
 }
 
+/*
+** For this one 1 is z, 2 os j, 3 is h, 5 is hh, 4 is l, 6 is ll.
+*/
+
+void ft_args(char *format, int *i, t_flags *tools)
+{
+    while (format[*i] != '\0' && (format[*i] == 'h' || format[*i] == 'l' ||
+            format[*i] == 'z' || format[*i] == 'j'))
+            {
+                if (format[*i] == 'z')
+                    tools->arg = 1;
+                else if (format[*i] == 'j') 
+                    tools->arg = 2;
+                else if (format[*i] == 'h') 
+                {
+                    if (tools->arg == 3)
+                        tools->arg = 5;
+                    else
+                        tools->arg = 3;
+                }
+                else if (format[*i] == 'l')
+                {
+                    if (tools->arg == 4)
+                        tools->arg = 6;
+                    else
+                        tools->arg = 4;
+                }
+                i += 1;
+            }
+}
