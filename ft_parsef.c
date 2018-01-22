101ba82694c0561b21469cbda0351a5d70a5bd05
Liam Prior <liamprior44@gmail.com>
2018-01-21 17:23:39 -0800

    adding more parsing stuff

--- ft_parsef.c
+++ ft_parsef.c
@@ -6,18 +6,88 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 14:21:44 by lprior            #+#    #+#             */
-/*   Updated: 2018/01/20 11:59:57 by lprior           ###   ########.fr       */
+/*   Updated: 2018/01/21 17:21:33 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void fwpa(char *format, int *i, va_list ap, t_flags *sack)
+// void	type(char t, t_flags *bag)
+// {
+// 	if (t == 'd' || t == 's' || t == 'S' || t == 'p' || t == 'D' ||
+// 		t == 'i' || t == 'o' || t == 'O' || t == 'u' || t == 'X' ||
+// 		t == 'U' || t == 'x' || t == 'c' || t == 'C' || t == 'E' ||
+// 		t == 'e' || t == 'f' || t == 'F' || t == '%' || t == 'b')
+// 		TYPE = t;
+// 	else
+// 		TYPE = -1;
+// }
+
+//sSpdDioOuUxXcC
+
+void ft_type2(char format, t_flags *tools)
 {
-    arguments(format, i, sack);
-	width(format, i, sack, ap);
-	precision(format, i, sack, ap);
-    flags(format, i, sack);
+	 if (format == 'X')
+		tools->brand = format;
+	else if (format == 'c')
+		tools->brand = format;
+	else if (format == 'C')
+		tools->brand = format;
+	else if (format == '%')
+		tools->brand = format;
+	else
+		tools->brand = -1;
+}
+
+void ft_type(char format, t_flags *tools)
+{
+	if (format == 's')
+		tools->brand = format;
+	else if (format == 'S')
+		tools->brand = format;
+	else if (format == 'p')
+		tools->brand = format;
+	else if (format == 'd')
+		tools->brand = format;
+	else if (format == 'D')
+		tools->brand = format;
+	else if (format == 'i')
+		tools->brand = format;
+	else if (format == 'o')
+		tools->brand = format;
+	else if (format == 'O')
+		tools->brand = format;
+	else if (format == 'u')
+		tools->brand = format;
+	else if (format == 'U')
+		tools->brand = format;
+	else if (format == 'x')
+		tools->brand = format;
+	else
+		ft_type2(format, tools);
+}
+
+void ft_fwap(char *format, int *i, va_list ap, t_flags *tools)
+{
+    ft_args(format, i, tools);
+	ft_width(format, i, tools);
+	ft_prec(format, i, tools, ap);
+    ft_flags(format, i, tools);
+}
+
+void parse_flags2(char *format, int *i, va_list ap, t_flags *tools)
+{
+	if (format[*i] != '\0')
+	{
+		if (format[*i] == 'h')
+			ft_fwap(format, i, tools, ap);
+		else if (format[*i] == 'l')
+			ft_fwap(format, i, tools, ap);
+		else if (format[*i] == 'j')
+			ft_fwap(format, i, tools, ap);
+		else if (format[*i] == 'z')
+			ft_fwap(format, i, tools, ap);
+	}
 }
 
 void parse_flags(char *format, int *i, va_list ap, t_flags *tools)
@@ -25,36 +95,21 @@ void parse_flags(char *format, int *i, va_list ap, t_flags *tools)
 	if (format[*i] != '\0')
 	{
 		if (format[*i] == '.')
-			fwpa(format, i,  tools, ap);
+			ft_fwap(format, i,  tools, ap);
 		else if (format[*i] == '-')
-			fwpa(format, i, tools, ap);
+			ft_fwap(format, i, tools, ap);
 		else if (format[*i] == '+')
-			fwpa(format, i, tools, ap);
+			ft_fwap(format, i, tools, ap);
 		else if (format[*i] == ' ')
-			fwpa(format, i, tools, ap);
+			ft_fwap(format, i, tools, ap);
 		else if (format[*i] == '#')
-			fwpa(format, i, tools, ap);
+			ft_fwap(format, i, tools, ap);
 		else if (format[*i] == '0')
-			fwpa(format, i, tools, ap);
-		else if (format[*i] >= '0' && format[*i] <= '9')
-			fwpa(format, i, tools, ap);
+			ft_fwap(format, i, tools, ap);
+		else if (format[*i] >= '0' && format[*i] <= '9')//width call
+			ft_fwap(format, i, tools, ap);
 		else 
 			parse_falgs2(format, &i,  tools, ap);
 	}
-    type(format[*i], tools);
-}
-
-void parse_flags2(char *format, int *i, va_list ap, t_flags *sack)
-{
-	if (format[*i] != '\0')
-	{
-		if (format[*i] == 'h')
-			fwpa(format, i, sack, ap);
-		else if (format[*i] == 'l')
-			fwpa(format, i, sack, ap);
-		else if (format[*i] == 'j')
-			fwpa(format, i, sack, ap);
-		else if (format[*i] == 'z')
-			fwpa(format, i, sack, ap);
-	}
+    ft_type(format[*i], tools);
 }
