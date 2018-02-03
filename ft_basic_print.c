80c0a7ec673034e587655d23e5b3eec7cdd40b88
Liam Prior <liamprior44@gmail.com>
2018-02-03 11:57:28 -0800

    walking away from keyboard

--- ft_basic_print.c
+++ ft_basic_print.c
@@ -6,7 +6,7 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 15:23:13 by lprior            #+#    #+#             */
-/*   Updated: 2018/02/02 18:14:57 by lprior           ###   ########.fr       */
+/*   Updated: 2018/02/03 11:56:24 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 // 	x = (unsigned char)va_arg(ap, int);
 // 	LEN = 1;
 // 	parse(bag);
-// 	while (MINUS == false && WIDTH-- > 0)
-// 		ZERO == true ? ft_putchar('0', bag) : ft_putchar(' ', bag);
+	// while (MINUS == false && WIDTH-- > 0)
+		// ZERO == true ? ft_putchar('0', bag) : ft_putchar(' ', bag);
 // 	ft_putchar(x, bag);
 // 	while (WIDTH-- > 0)
 // 		ft_putchar(' ', bag);
@@ -43,9 +43,18 @@ void ft_print_int(t_flags *tools, va_list ap)
 
 void ft_print_char(t_flags *tools, va_list ap)
 {
-    unsigned char x;
+    unsigned char leter;
 
-    x = va_arg(ap, unsigned char);
+    leter = va_arg(ap, unsigned char);
     parse_tools(tools);
-
+    while (tools->negative == 0 && tools->width-- > 0)
+    {  
+        if (tools->zeros == 0)
+             ft_putchar('0');
+        else
+            ft_putchar(' ');
+    }
+    ft_putchar(leter);
+    while (tools->width-- > 0)
+        ft_putchar(' ');
 }
