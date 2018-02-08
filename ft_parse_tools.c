94b02b0a09cd57602a800eef7b015591dfae3053
Liam Prior <liamprior44@gmail.com>
2018-02-07 17:38:17 -0800

    working precision and width

--- ft_parse_tools.c
+++ ft_parse_tools.c
@@ -6,7 +6,7 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 19:29:14 by lprior            #+#    #+#             */
-/*   Updated: 2018/02/07 16:41:59 by lprior           ###   ########.fr       */
+/*   Updated: 2018/02/07 17:37:30 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,10 @@
 // }
 void    ft_parse_char_tool(t_flags *tools)
 {
-    // printf("len = [%d]\n", tools->len);
     if (tools->prec < tools->len && tools->prec >= 0)
         tools->len = tools->prec;
-    if (tools->width < 0 && tools->brand == 'c')
-        tools->width *= -1;
-    if (tools->prec >= 0)
+    if (tools->prec >= 0 || tools->width >= 0)
         tools->width -= tools->len;
-    // if (tools->width >= 0)
-    //     tools->width -= tools->len;
-    if (tools->prec >= 0 && tools->prec == 0 && tools->brand == 'c')
-        tools->width -= 1;
 }
 // void			parse_int(t_flags *bag, long int nb)
 // {
