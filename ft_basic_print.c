fd105119fab348b1f68005c549cd20fb8037aecd
Liam Prior <liamprior44@gmail.com>
2018-02-09 12:33:51 -0800

    printing i and d works 100%

--- ft_basic_print.c
+++ ft_basic_print.c
@@ -6,7 +6,7 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 15:23:13 by lprior            #+#    #+#             */
-/*   Updated: 2018/02/08 18:29:21 by lprior           ###   ########.fr       */
+/*   Updated: 2018/02/09 12:20:02 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void ft_print_string(t_flags *tools, va_list ap)
     char *string;
 
     string = va_arg(ap, char *);
-    // str = (str == NULL ? "(null)" : str);
+    if (string == NULL)
+        string = "(NULL)";
     tools->len = ft_strlen(string);
     ft_parse_char_tool(tools);
     while (tools->negative == 0 && tools->width-- > 0)
@@ -27,7 +28,7 @@ void ft_print_string(t_flags *tools, va_list ap)
         else
             ft_putchar(' ');
     }
-    while ((tools->len-- > 0) && *string)
+    while ((tools->len-- > 0) && *string && tools->prec != -100)
         ft_putchar(*string++);
     while (tools->width-- > 0)
     {
@@ -72,19 +73,15 @@ void ft_print_int2(t_flags *tools, long int number, int dis)//if i pass number t
         number *= -1;
         tools->positive = 0;
     }
-    // if (tools->prec == 0 && dis == 0)
-    //     ft_putchar(' ');
     ft_print_prec(tools);
-    // dis = 0;
-    // if (dis == 1 && number == 0)//if i do this then the top half works.
-    // {
-    //     if (tools->zeros == 1)
-    //         ft_putchar('0');
-    //     else if (tools->zeros == 0)
-    //         ft_putchar(' ');
-    // }
-    // else
-    if (number == 0 && dis == 0)//so if i do this then the bottum half works.
+    if (dis == 1 && number == 0)//if i do this then the top half works.
+    {
+        if (tools->zeros == 1)
+            ft_putchar('0');
+        else if (tools->prec == -100)
+            ft_putchar(' ');
+    }
+    else
         ft_putnbr(number);
     while (tools->width-- >= 1)
         ft_putchar(' ');
@@ -97,7 +94,7 @@ void ft_print_int(t_flags *tools, va_list ap)
     
     if ((number = ft_sort_signed_args(tools, ap)) == 0)
         number = ft_check_signed_int(tools, ap);
-    dis = tools->prec > -1 ? 1 : 0;
+    dis = (tools->prec > -1 || tools->prec == -100) ? 1 : 0;
     ft_parse_int_tools(tools, number);
     if (tools->zeros == 1 && tools->positive == 1 && number > -1)
     {
